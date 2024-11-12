622a69434b7e483b04cc0b8469e8bfeb1c743244
Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>
2024-11-12 18:39:25 +0530

    added comments for understandig

--- app/database/session.py
+++ app/database/session.py
@@ -1,3 +1,6 @@
-from app.schemas.item import Item
+# importing Item pydantic model from schemas
+from app.schemas.item import Item 
+
+# Dictionary as fake database  with 2 Item object as initial data
 fake_db={1:Item(id=1,description="first smartphone with 100MP camera",name="VIVO",price=20000,quantity=17),
          2:Item(id=2,description="new cooling system for better gaming experience",name="ASUS",price=40000,quantity=3)}
