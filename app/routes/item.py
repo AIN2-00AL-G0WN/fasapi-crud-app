507a009614b1d55f3289291b7d1ea6fe4e712a7e
tejasBhatGitHUb <107117704+tejasBhatGitHUb@users.noreply.github.com>
2024-11-12 20:05:30 +0530

    created CRUD apis for item (#4)
    * created CRUD apis for item
    
    * documented all the api endpoints.
    
    ---------
    
    Co-authored-by: Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>

---
+++ app/routes/item.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter,HTTPException
+from app.database.session import fake_db
+from app.schemas.item import CreateItem,UpdateItem,Item
+from typing import List,Any
+from fastapi.encoders import jsonable_encoder
+# creating router for item
+route=APIRouter()
+
+# root url 
+@route.get('/')
+def root():
+    return {"message":"Hello World"}
+# url to create an Item object.
+# first it checks if the item with the given id is present or not if present thows as HTTP exception otherwise creates an Item object and returns it.
+@route.post('/items')
+def create_item(item:CreateItem)->Item:
+    if item.id in fake_db:
+        raise HTTPException(status_code=400,detail="Item with this name or Id already exists in our Database.")
+    fake_db[item.id]=jsonable_encoder(item)
+    return item
+
+# url to retrive all the objects in our database
+@route.get('/items')
+def get_all_items()->dict[Any,Any]:
+    return fake_db
+
+# url to update an Item with specific id .
+# first it checks if the item with the given id is present or not if present updates the requested fields and return the updated object otherwise throws HTTP exception.
+@route.patch('/items/{id}')
+def update_item(id:int,item:UpdateItem)->Item:
+    if id in fake_db:
+        db_item_data=  fake_db[id]
+        db_item_model= CreateItem(**db_item_data)
+        updated_data= item.model_dump(exclude_unset=True)
+        updated_item= db_item_model.copy(update=updated_data)
+        fake_db[id]=  jsonable_encoder(updated_item)
+        return updated_item
+    raise HTTPException(status_code=404 , detail="Item not found put error")
+
+#url to delete item with specific id.
+# first it checks if the item with the given id is present or not if present deletes the  object otherwise throws HTTP exception.
+@route.delete('/items/{id}')
+def delete_item(id:int):
+    if id in fake_db:
+        del fake_db[id]
+        return {"message": "Done"}
+    raise HTTPException(status_code=404,detail="Item not found delete error")
