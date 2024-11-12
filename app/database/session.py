a10cc36459fce043bf50183873b5c6d5269dc15c
Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>
2024-11-12 18:36:34 +0530

    created a new database package and created fake database with 2 Item objects

---
+++ app/database/session.py
@@ -0,0 +1,3 @@
+from app.schemas.item import Item
+fake_db={1:Item(id=1,description="first smartphone with 100MP camera",name="VIVO",price=20000,quantity=17),
+         2:Item(id=2,description="new cooling system for better gaming experience",name="ASUS",price=40000,quantity=3)}
