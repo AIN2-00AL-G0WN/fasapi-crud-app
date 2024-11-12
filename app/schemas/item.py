a0a24769d002fbf144c0fa529032fa2997a3be20
tejasBhatGitHUb <107117704+tejasBhatGitHUb@users.noreply.github.com>
2024-11-12 19:30:49 +0530

    did the documentation for Item schema (#3)
    Co-authored-by: Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>

--- app/schemas/item.py
+++ app/schemas/item.py
@@ -1,7 +1,9 @@
+# Schema for pydantic model
+# importing pydantic Base model as parent which is inherited by all the model classes
 from pydantic import BaseModel
 from typing import Optional
 
-
+# Base model for Item 
 class ItemBase(BaseModel):
     id:int
     name:str
@@ -9,19 +11,18 @@ class ItemBase(BaseModel):
     price:float
     quantity:int
 
-    class Config:
-        orm_mode = True
-
+#class to create Item model
 class CreateItem(ItemBase):
     pass
 
+# class used return Item model when requested
 class Item(ItemBase):
     pass
 
+# class to update the model.
+# only description, price and quantity of the model can be modeified all these fields are optional
 class UpdateItem(BaseModel):
     description:Optional[str]=None
     price:Optional[float]=None
     quantity:Optional[int]=None
 
-    class Config:
-        orm_mode=True
