6920bd6948bc064ee25a875ce34212a65492aed1
tejasBhatGitHUb <107117704+tejasBhatGitHUb@users.noreply.github.com>
2024-11-13 16:26:03 +0530

    removed CreateItem import which was giving error (#13)
    Co-authored-by: Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>

--- app/crud/item/create.py
+++ app/crud/item/create.py
@@ -1,6 +1,9 @@
 from fastapi.encoders import jsonable_encoder
+from app.schemas.item import Item
 
-def create(item,db):
+# this function takes Item object and db session. 
+# checks for Item with given id if present throws an exception otherwise creates the Item and return it.
+def create(item:Item,db)->Item:
     if item.id not in db:
         db[item.id]=jsonable_encoder(item)
         return item
