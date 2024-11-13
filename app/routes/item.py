547211d893f3165fc4c2f8b81be5ab9db8c7c63c
tejasBhatGitHUb <107117704+tejasBhatGitHUb@users.noreply.github.com>
2024-11-13 16:21:22 +0530

    reorganized codes using crud functions (#12)
    Co-authored-by: Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>

--- app/routes/item.py
+++ app/routes/item.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter,HTTPException
 from app.database.session import fake_db
-from app.schemas.item import CreateItem,UpdateItem,Item
-from typing import List,Any
-from fastapi.encoders import jsonable_encoder
+from app.schemas.item import UpdateItem,Item
+from typing import Any
+from app.crud.item import create,read,update,delete
+
 # creating router for item
 route=APIRouter()
 
@@ -10,46 +11,44 @@ route=APIRouter()
 @route.get('/')
 def root():
     return {"message":"Hello World"}
+
 # url to create an Item object.
 # first it checks if the item with the given id is present or not if present thows as HTTP exception otherwise creates an Item object and returns it.
 @route.post('/items')
-def create_item(item:CreateItem)->Item:
-    if item.id in fake_db:
-        raise HTTPException(status_code=400,detail="Item with this name or Id already exists in our Database.")
-    fake_db[item.id]=jsonable_encoder(item)
-    return item
+def create_item(item:Item)->Item:
+    try:
+        return create.create(item,fake_db)
+    except:
+        raise HTTPException(detail="This item is already  present in our database",status_code=400)
 
 # url to retrive all the items in our database
 @route.get('/items')
-def get_all_items()->dict[Any,Any]:
-    return fake_db
+def get_all_items()->dict[Any,Item]:
+    return read.get_all_items(fake_db)
 
 # url to retrive perticular item
 # first it checks if the item with the given id is present or not if present returns the Item,otherwise thows a HTTP exception.
 @route.get('/items/{id}')
 def get_item(id:int)->Item:
-    if id in fake_db:
-        return fake_db[id]
-    raise HTTPException(status_code=404 , detail="Item not found put error")
+    try:
+        return read.get_item(id,fake_db)
+    except:
+        raise HTTPException(status_code=404 , detail="Item not found put error")
 
 # url to update an Item with specific id .
 # first it checks if the item with the given id is present or not if present updates the requested fields and return the updated object otherwise throws HTTP exception.
 @route.put('/items/{id}')
 def update_item(id:int,item:UpdateItem)->Item:
-    if id in fake_db:
-        db_item_data=  fake_db[id]
-        db_item_model= CreateItem(**db_item_data)
-        updated_data= item.dict(exclude_unset=True)
-        updated_item= db_item_model.copy(update=updated_data)
-        fake_db[id]=  jsonable_encoder(updated_item)
-        return updated_item
-    raise HTTPException(status_code=404 , detail="Item not found put error")
+    try:
+        return update.update(id,item,fake_db)
+    except:
+        raise HTTPException(status_code=404 , detail="Item not found put error")
 
 #url to delete item with specific id.
 # first it checks if the item with the given id is present or not if present deletes the  object otherwise throws HTTP exception.
 @route.delete('/items/{id}')
 def delete_item(id:int):
-    if id in fake_db:
-        del fake_db[id]
-        return {"message": "Item deleted successfully."}
-    raise HTTPException(status_code=404,detail="Item not found delete error")
+    try:
+        return delete.delete(id,fake_db)
+    except:
+        raise HTTPException(status_code=404,detail="Item not found delete error")
