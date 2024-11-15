0ed8dc5167e7bb6a05e7045baa387b75aa09ef1d
Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>
2024-11-15 16:49:21 +0530

    implemented delete function

--- app/crud/item/delete.py
+++ app/crud/item/delete.py
@@ -1,9 +1,10 @@
-from app.schemas.item import Item
+from sqlalchemy.orm import session
+from app.models.item import Item as item_model
+from app.schemas.item import CreateItem as create_item_schema,UpdateItem as update_item_schema
 
 # this function takes an id and db session. 
-# checks for Item with given id if present deletes the Item and return a success message  otherwise throws an exception.
-def delete(id:Item,db)-> dict:
-    if id in db:
-        del db[id]
-        return {"message": "Item deleted successfully."}
-    raise FileExistsError(f"Item with id {id} not found in our database")
+# checks for Item with given id if present deletes the Item otherwise throws an exception.
+def delete(db:session,id:int):
+    item=db.query(item_model).get(id)
+    db.delete(item)
+    db.commit()
