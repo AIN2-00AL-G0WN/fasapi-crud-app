7365ef758ff0a6440433492a0f8f24464fe522ad
tejasBhatGitHUb <107117704+tejasBhatGitHUb@users.noreply.github.com>
2024-11-15 17:17:23 +0530

    reimplemented item schema (#22)
    Co-authored-by: Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>

--- app/schemas/item.py
+++ app/schemas/item.py
@@ -1,16 +1,27 @@
-# Schema for pydantic model
-# importing pydantic Base model as parent which is inherited by all the model classes
 from pydantic import BaseModel
 from typing import Optional
 
-#class to create Item model
+
+# model to create Item
+class CreateItem(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    quantity: int
+
+    class Config:
+        orm_mode = True  # to serialize and deserialize sql models
+
+# Item model
 class Item(BaseModel):
     id:int
-    name:str
-    description:Optional[str]=None
-    price:float
-    quantity:int
+    name: str
+    description: Optional[str] = None
+    price: float
+    quantity: int
 
+    class Config:
+        orm_mode = True  # to serialize and deserialize sql models
 
 # class to update the model.
 # only description, price and quantity of the model can be modeified all these fields are optional
@@ -18,3 +29,6 @@ class UpdateItem(BaseModel):
     description:Optional[str]=None
     price:Optional[float]=None
     quantity:Optional[int]=None
+
+    class Config:
+        orm_mode=True  # to serialize and deserialize sql models
