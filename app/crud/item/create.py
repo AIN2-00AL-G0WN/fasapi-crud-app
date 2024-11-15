ee67d8d72743156b7e77bd9417b656395060c18c
tejasBhatGitHUb <107117704+tejasBhatGitHUb@users.noreply.github.com>
2024-11-15 16:46:46 +0530

    created create function with real db session (#17)
    Co-authored-by: Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>

--- app/crud/item/create.py
+++ app/crud/item/create.py
@@ -1,10 +1,12 @@
-from fastapi.encoders import jsonable_encoder
-from app.schemas.item import Item
+from sqlalchemy.orm import session
+from app.models.item import Item as item_model
+from app.schemas.item import CreateItem as create_item_schema
 
 # this function takes Item object and db session. 
-# checks for Item with given id if present throws an exception otherwise creates the Item and return it.
-def create(item:Item,db)->Item:
-    if item.id not in db:
-        db[item.id]=jsonable_encoder(item)
-        return item
-    raise ValueError("Item with this name or Id already exists in our Database.")
+# checks for Item with given name if present throws an exception otherwise creates the Item and return it.
+def create(db:session,item:create_item_schema):
+    db_item=item_model(name=item.name,description=item.description,price=item.price,quantity=item.quantity)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
