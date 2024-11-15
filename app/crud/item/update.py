9332ae9b4a6d845ea9a6b753861a7abe7af2aaad
tejasBhatGitHUb <107117704+tejasBhatGitHUb@users.noreply.github.com>
2024-11-15 17:01:10 +0530

    implemented update function (#20)
    Co-authored-by: Tejas B. Gopal <Tejas.Gopalbhat@cxtorg.net>

--- app/crud/item/update.py
+++ app/crud/item/update.py
@@ -1,14 +1,20 @@
-from app.schemas.item import Item
-from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import session
+from app.models.item import Item as item_model
+from app.schemas.item import UpdateItem as update_item_schema
 
-# this function takes Item id and db session as parameter
+# this function takes Item id, db session and item_update as parameter
 # it checks for ihe item with given id. If present updates the item and returns the updated Item otherwise throws an exception.
-def update(id:int,item,db)->Item:
-    if id in db:
-        db_item_data=  db[id]
-        db_item_model= Item(**db_item_data)
-        updated_data= item.dict(exclude_unset=True)
-        updated_item= db_item_model.copy(update=updated_data)
-        db[id]=  jsonable_encoder(updated_item)
-        return updated_item
-    raise FileExistsError(detail="Item not found")
+def update(db:session,id:int,item_update:update_item_schema):
+    db_item=db.query(item_model).get(id)
+
+    if item_update.description is not None:
+        db_item.description=item_update.description
+
+    if item_update.price is not None:
+        db_item.price=item_update.price
+
+    if item_update.quantity is not None:
+        db_item.quantity=item_update.quantity
+    db.commit()
+    db.refresh(db_item)
+    return db_item
